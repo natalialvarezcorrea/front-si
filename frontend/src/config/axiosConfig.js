a266de6d5857d29a748281cbccdd0cf6b1bf6d92
natalialvarezcorrea <natalia.37.na@gmail.com>
2019-11-27 14:36:42 -0500

    actual

--- frontend/src/config/axiosConfig.js
+++ frontend/src/config/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosFetch = axios.create({
-    baseURL: 'http://35.198.30.114:5000'
+    baseURL: 'http://34.95.213.121:5000/'
 });
 
 export default axiosFetch;
